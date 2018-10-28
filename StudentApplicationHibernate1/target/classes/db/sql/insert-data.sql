7f401fc1440316a87f782a3846c41bf9bc91a592
pradeep544 <pradeep544@users.noreply.github.com>
2018-10-29 04:52:24 +0530

    direct upload

---
+++ StudentApplicationHibernate1/target/classes/db/sql/insert-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO students VALUES (1, 'mkyong', 'kukkala');
+INSERT INTO students VALUES (2, 'alex', 'hello');
+INSERT INTO students VALUES (3, 'joel', 'test');
+INSERT INTO students VALUES (4, 'noel', 'mist');
